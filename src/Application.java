905f384cc802f305f356dd7ad1b08969f8b49deb
arjunhebbar007 <arjunh0007@gmail.com>
2022-01-21 12:26:21 -0500

    Created sample program

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+    public static void main(String[] args){
+        int a =5;
+
+        for (int i = 0; i <10; i=i+1)
+        {
+            System.out.printf("Value is: "+i+"\n");
+        }
+
+
+
+}}
