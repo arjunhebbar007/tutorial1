cef72195f3e8debd7f8a8056e6ea4594ac1a4b08
arjunhebbar007 <arjunh0007@gmail.com>
2022-01-24 17:25:05 -0500

    Created sample program

---
+++ src/Username.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Username {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Username: ");
+        System.out.println("Enter Password: ");
+
+        String username = sc.next();
+        String password = sc.next();
+
+        if(username.equals("Arjun")&password.equals("12345"))
+            System.out.println("Login Successful");
+        else
+            System.out.println("Invalid username and pass");
+
+
+    }
+}
